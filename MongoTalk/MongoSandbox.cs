d9457633f8ea1105ecf444e48ec532d697479747
Nate Buwalda <buwaldanate@johndeere.com>
2014-03-28 22:11:21 -0500

    Insert and removal basics

--- MongoTalk/MongoSandbox.cs
+++ MongoTalk/MongoSandbox.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using log4net;
 using log4net.Config;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
 
 namespace MongoTalk
 {
@@ -27,6 +29,12 @@ namespace MongoTalk
             FindNateAsBsonDocument(database);
             FindNateAsEmployeeObject(database);
             FindOldSoftwareEmployees(database);
+
+            Log.Info("Inserting new documents");
+            InsertBsonDocument(database);
+
+            Log.Info("Removing everyone except Nate");
+            RemoveEveryoneExceptNate(database);
 
             Log.Info("Done running Mongo Sandbox");
             Console.WriteLine("Press any key to continue...");
@@ -61,7 +69,7 @@ namespace MongoTalk
             var employeeDocument = domainCollection.FindOne(Query.EQ("name", "Nate Buwalda"));
             Log.InfoFormat("Employee document version is: {0}", employeeDocument);
         }
-        
+
         private static void FindOldSoftwareEmployees(MongoDatabase database)
         {
             var domainCollection = database.GetCollection<Employee>("employee");
@@ -75,6 +83,29 @@ namespace MongoTalk
             {
                 Log.InfoFormat("{0} is an old software developer", employee.Name);
             }
+        }
+
+        private static void InsertBsonDocument(MongoDatabase database)
+        {
+            var bsonCollection = database.GetCollection<BsonDocument>("employee");
+            var bsonDocument = new BsonDocument
+            {
+                {"name", "Stan Smith"},
+                {"department", "Intelligence"},
+                {"age", 45}
+            };
+            bsonCollection.Insert(bsonDocument);
+            var insertedDocument = bsonCollection.FindOne(Query.EQ("name", "Stan Smith"));
+            Log.InfoFormat("The inserted bson document was {0}", insertedDocument);
+        }
+
+        private static void RemoveEveryoneExceptNate(MongoDatabase database)
+        {
+            var employeeCollection = database.GetCollection<Employee>("employee");
+            employeeCollection.Remove(Query.NE("name", "Nate Buwalda"));
+
+            var documentsInCollection = employeeCollection.AsQueryable().Count();
+            Log.InfoFormat("There were {0} document(s) left in the employee collection after the removal", documentsInCollection);
         }
     }
 }
